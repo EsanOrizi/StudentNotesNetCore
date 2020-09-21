82c3da86bac006e7ec89c83d3760b625b570085f
EsanEm <esanem@outlook.com>
2020-09-21 23:30:58 +0100

    Data seed removed

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -17,15 +17,6 @@ namespace Persistence
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            // builder.Entity<Student>()
-            // .HasData(
-            //     new Student { Name = "Ehsan", Address = "11 Harold Road", Phone = "123456" },
-            //     new Student { Name = "Mahsa", Address = "22 Hastings Road", Phone = "321654" },
-            //     new Student { Name = "Poyan ", Address = "44 Denzil Avenue", Phone = "789987" },
-            //     new Student { Name = "Sam", Address = "10 Tessa Court", Phone = "456654" },
-            //     new Student { Name = "Dash", Address = "9 Cindy Court", Phone = "987123" }
-            // );
-
             builder.Entity<Student>()
             .HasMany(s => s.Notes)
             .WithOne(n => n.Student)
