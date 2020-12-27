1aecaf2467eec5d27e4de0f3e5967c98de49e00e
EsanEm <esanem@outlook.com>
2020-12-27 22:42:58 +0000

    Base controller, User controller added

--- API/Controllers/StudentsController.cs
+++ API/Controllers/StudentsController.cs
@@ -9,45 +9,39 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
 
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StudentsController : ControllerBase
+    public class StudentsController : BaseController
     {
-        private readonly IMediator _mediator;
-        public StudentsController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+
 
         [HttpGet]
         public async Task<ActionResult<List<Student>>> List()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Student>> Details(Guid id)
         {
-            return await _mediator.Send(new Details.Query { Id = id });
+            return await Mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
-            return await _mediator.Send(command);
+            return await Mediator.Send(command);
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
         {
             command.Id = id;
-            return await _mediator.Send(command);
+            return await Mediator.Send(command);
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
-            return await _mediator.Send(new Delete.Command { Id = id });
+            return await Mediator.Send(new Delete.Command { Id = id });
         }
 
     }
