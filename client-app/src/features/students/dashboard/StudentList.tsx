a84f488a2802939e335a7ecea4a12eb6e6fccb6d
EsanEm <esanem@outlook.com>
2021-02-19 00:42:52 +0000

    delete student fix

--- client-app/src/features/students/dashboard/StudentList.tsx
+++ client-app/src/features/students/dashboard/StudentList.tsx
@@ -1,12 +1,5 @@
 import React, { useContext, useState } from "react";
-import {
-  Item,
-  Button,
-  Segment,
-  Modal,
-  Icon,
-  Header,
-} from "semantic-ui-react";
+import { Item, Button, Segment, Modal, Icon, Header } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
 import { RootStoreContext } from "../../../app/stores/rootStore";
@@ -15,11 +8,8 @@ const StudentList: React.FC = () => {
   const rootStore = useContext(RootStoreContext);
   const {
     studentArrayFromMap,
-    deleteStudent,
-    submitting,
-     } = rootStore.mobxStore;
+  } = rootStore.mobxStore;
 
-  const [open, setOpen] = useState(false);
 
   return (
     <Segment clearing>
@@ -41,32 +31,6 @@ const StudentList: React.FC = () => {
                   content="View"
                   color="blue"
                 />
-                <Modal
-                  open={open}
-                  size='mini'
-                  trigger={
-                    <Button floated="right" content="Delete" color="red" />
-                  }
-                  onClose={() => setOpen(false)}
-                  onOpen={() => setOpen(true)}
-                >
-                  <Header content="Delete student?" />
-                  <Modal.Content>
-                    <p>Are you sure you like to delete this student?</p>
-                  </Modal.Content>
-                  <Modal.Actions>
-                    <Button
-                      color="red"
-                      loading={submitting}
-                      onClick={(e) => deleteStudent(e, student.id)}
-                    >
-                      <Icon name="remove" /> YES DELETE
-                    </Button>
-                    <Button color="green" onClick={() => setOpen(false)}>
-                      <Icon name="checkmark" /> No
-                    </Button>
-                  </Modal.Actions>
-                </Modal>
               </Item.Extra>
             </Item.Content>
           </Item>
--- client-app/src/features/students/details/StudentDetails.tsx
+++ client-app/src/features/students/details/StudentDetails.tsx
@@ -1,23 +1,33 @@
-import React, { useContext, useEffect } from 'react';
-import { Card, Button } from 'semantic-ui-react';
-import { observer } from 'mobx-react-lite';
-import { RouteComponentProps, Link } from 'react-router-dom';
-import LoadingComponent from '../../../app/layout/LoadingComponent';
-import { RootStoreContext } from '../../../app/stores/rootStore';
+import React, { useContext, useEffect, useState } from "react";
+import { Card, Button, Modal, Header, Icon } from "semantic-ui-react";
+import { observer } from "mobx-react-lite";
+import { RouteComponentProps, Link } from "react-router-dom";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { RootStoreContext } from "../../../app/stores/rootStore";
 
 interface DetailParams {
   id: string;
 }
 
-const StudentDetails: React.FC<RouteComponentProps<DetailParams>> = ({ match, history }) => {
+const StudentDetails: React.FC<RouteComponentProps<DetailParams>> = ({
+  match,
+  history,
+}) => {
   const rootStore = useContext(RootStoreContext);
-  const { student, loadStudent, loadingInitial } = rootStore.mobxStore;
+  const {
+    student,
+    loadStudent,
+    loadingInitial,
+    deleteStudent,
+    submitting,
+  } = rootStore.mobxStore;
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     loadStudent(match.params.id);
   }, [loadStudent, match.params.id, history]);
 
-  if (loadingInitial) return <LoadingComponent content='Loading student' />;
+  if (loadingInitial) return <LoadingComponent content="Loading student" />;
 
   if (!student) return <h2>Student Not Found</h2>;
 
@@ -30,27 +40,55 @@ const StudentDetails: React.FC<RouteComponentProps<DetailParams>> = ({ match, hi
           <Card.Description>{student!.phone}</Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <Button.Group widths={3}>
+          <Button.Group widths={4}>
             <Button
               as={Link}
               to={`/manageStudent/${student.id}`}
               basic
-              color='blue'
-              content='Edit'
+              color="blue"
+              content="Edit"
             />
             <Button
-              onClick={() => history.push('/students')}
+              onClick={() => history.push("/students")}
               basic
-              color='grey'
-              content='Cancel'
+              color="grey"
+              content="Cancel"
             />
             <Button
               as={Link}
               to={`/studentNotes/${student.id}`}
               basic
-              color='blue'
-              content='Notes'
+              color="blue"
+              content="Notes"
             />
+            <Modal
+              open={open}
+              size="mini"
+              trigger={<Button floated="right" content="Delete" color="red" />}
+              onClose={() => setOpen(false)}
+              onOpen={() => setOpen(true)}
+            >
+              <Header content="Delete student?" />
+              <Modal.Content>
+                <p>Are you sure you like to delete this student?</p>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button
+                  color="red"
+                  loading={submitting}
+                  onClick={(e) =>
+                    deleteStudent(e, student.id)
+                      .finally(() => setOpen(false))
+                      .finally(() => history.push("/students"))
+                  }
+                >
+                  <Icon name="remove" /> YES DELETE
+                </Button>
+                <Button color="green" onClick={() => setOpen(false)}>
+                  <Icon name="checkmark" /> No
+                </Button>
+              </Modal.Actions>
+            </Modal>
           </Button.Group>
         </Card.Content>
       </Card>
